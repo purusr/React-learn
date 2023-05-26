f10da22626aacbf399297097d1a6185b41f61fd6
purusr <rankrish13@gmail.com>
2023-05-26 11:48:02 +0530

    changing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Test from './components/Test';
 import Card from './components/Card';
 
 function App() {      
-  console.log('On learn branch')
   // const [starWarsData, setStarWarsData] = React.useState({})
   
   // React.useEffect(function() {
